50866b311b69afca2d286709fff733ed6d1c58db
Michael Baterna <micheal02052007@gmail.com>
2020-06-16 16:24:11 +0800

    Remove add_virtualbox_vm

--- archlinux_deploy/stages/prepare.py
+++ archlinux_deploy/stages/prepare.py
@@ -13,13 +13,11 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from archlinux_deploy import VM_NAME, VM_MEM_SIZE, VM_VRAM_SIZE, VM_IGNORE_DUPLICATES, VBOX_VMS_LOCATION, VM_HDD_SIZE
 from archlinux_deploy.stages.exceptions import BaseStageException  # type: ignore
 from archlinux_deploy import utils
 from typing import List, Callable
 from bs4 import BeautifulSoup  # type: ignore
 from rich import print
-import virtualbox  # type: ignore
 import subprocess
 import requests
 import os
@@ -114,88 +112,8 @@ def sp_call(command: str, accepted_return_codes: List[int] = None, ignore_return
         )
     return return_code
 
-def create_virtualbox_vm():
-    vbox: virtualbox.VirtualBox = virtualbox.VirtualBox()
-    vm_names: List[str] = [machine.name for machine in vbox.machines]
-    if VM_NAME in vm_names:
-        if VM_IGNORE_DUPLICATES is False:
-            raise BaseStageException(
-                F"It seems like there's a vm that has the same name that was going to be created ({VM_NAME}).\n"
-                F"Either rename the VM, set the vm_name in config.ini to something else or set ignore_duplicates to 1 in the environment variables."
-            )
-        print(f"[yellow]There's a VM with the same name ({VM_NAME}). Continuing anyway...")
-
-    try:
-        vm: virtualbox.library.IMachine = vbox.create_machine(
-            name="arch-linux",
-            os_type_id="ArchLinux_64",
-            groups=["/"],
-            flags="",
-            settings_file=""
-        )
-        vbox.register_machine(vm)
-    except virtualbox.library_ext.library.VBoxErrorFileError:
-        vm = vbox.find_machine("arch-linux")
-    print("[blue]Created and registered Arch Linux VM." if "arch-linux" not in vm_names else "[blue]Got Arch Linux VM.")
-    print(f"[blue]Changing VRAM size to {VM_VRAM_SIZE} MB.")
-    try:
-        vm.vram_size = VM_VRAM_SIZE
-    except AttributeError:
-        print("[yellow]Couldn't change vram size using 'virtualbox' module, using subprocess instead")
-        sp_call(f"{vbox_manage_command()} modifyvm {VM_NAME} --vram {VM_VRAM_SIZE}")
-    print("[green]Operation successful.")
-    print(f"[blue]Changing memory size to {VM_MEM_SIZE} MB.")
-    try:
-        vm.memory_size = VM_MEM_SIZE
-    except Exception:  # virtualbox module raises a Exception exception
-        print("[yellow]Couldn't change memory size using 'virtualbox' module, using subprocess instead")
-        sp_call(f"{vbox_manage_command()} modifyvm {VM_NAME} --memory {VM_MEM_SIZE}")
-    print("[green]Operation successful.")
-    print("[blue]Changing graphics controller to vmsvga.")
-    try:
-        vm.graphics_controller_type = virtualbox.library.GraphicsControllerType.VMSVGA
-    except AttributeError:
-        print("[yellow]Couldn't change graphics controller type using 'virtualbox' module, using subprocess instead")
-        sp_call(f"{vbox_manage_command()} modifyvm {VM_NAME} --graphicscontroller vmsvga")
-    print("[green]Operation successful.")
-    # have no idea to use virtualbox module beyond this, using subprocess instead
-    print(f"[blue]Creating VDI file with the size of {VM_HDD_SIZE} MB.")
-    previous_dir: str = os.getcwd()
-    os.chdir(VBOX_VMS_LOCATION)
-    create_disk_rc: int = sp_call(
-        f"{vbox_manage_command()} createmedium disk --filename {os.path.join(VM_NAME, f'{VM_NAME}.vdi')} --size 20480 --format VDI --variant Standard",
-        accepted_return_codes=[0, 1]
-    )
-    if create_disk_rc == 1:
-        print("[yellow]VDI file already exists, continuing.")
-    print("[green]Operation successful.")
-    print("[blue]Adding SATA storage controller.")
-    add_storage_controller_rc: int = sp_call(f"{vbox_manage_command()} storagectl {VM_NAME} --name HDD --add sata --controller IntelAHCI --bootable on",
-                                             accepted_return_codes=[0, 1])
-    if add_storage_controller_rc == 1:
-        print("[yellow]SATA storage controller already exists, continuing.")
-    print("[green]Operation successful.")
-    print("[blue]Attaching HDD to SATA storage controller.")
-    sp_call(
-        f"{vbox_manage_command()} storageattach {VM_NAME} --storagectl HDD --device 0 --port 0 --type hdd --medium {os.path.join(VM_NAME, f'{VM_NAME}.vdi')}"
-    )
-    print("[green]Operation successful.")
-    print("[blue]Adding IDE storage controller, to mount the disk image.")
-    add_disk_controller_rc: int = sp_call(
-        f"{vbox_manage_command()} storagectl {VM_NAME} --name Disk_Image --add ide --bootable on",
-        accepted_return_codes=[0, 1]
-    )
-    if add_disk_controller_rc == 1:
-        print("[yellow]IDE storage controller already exists, continuing.")
-    print("[green]Operation successful.")
-    print("[blue]Attaching Arch Linux ISO to IDE controller.")
-    os.chdir(previous_dir)
-    sp_call(
-        f"{vbox_manage_command()} storageattach {VM_NAME} --storagectl Disk_Image --device 0 --port 0 --type dvddrive --medium arch-linux.iso"
-    )
-
 def run():
-    stages: List[Callable] = [check_for_virtualbox, check_for_vboxapi, check_for_xpcom, download_latest_iso, create_virtualbox_vm]
+    stages: List[Callable] = [check_for_virtualbox, check_for_vboxapi, check_for_xpcom, download_latest_iso]
     for stage in stages:
         stage_name: str = stage.__name__
         utils.colored_output(f"Stage 1 Substage {stage_name} started!", "info")
